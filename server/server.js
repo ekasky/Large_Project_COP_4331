08526215accd54c06d4ca2458fb58159ffe75527
Evan Kasky <ekasky25@gmail.com>
2024-06-13 23:52:17 -0400

    Adds json middleware
    	- Uses built in ExpressJS middleware to pass incoming request as
    	  json

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const app     = express();
 const port    = process.env.PORT;
 
+app.use(express.json());                    // Parse incoming requests as json
 
 app.listen(port, () => {
 
