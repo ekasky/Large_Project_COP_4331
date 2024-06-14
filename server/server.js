d598595374ae911a6227ed26caa8c0ae7fb2d2b4
Evan Kasky <ekasky25@gmail.com>
2024-06-14 00:06:39 -0400

    Adds api auth routes file
    	- Adds routes/api/authRoutes which will hold all the auth routes
    	  for the api
    
    	- For now has a test api endpoint to ensure proper connections
    
    	- Mounts the auth api in the main server.js file

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 require('dotenv').config();
-const express = require('express');
+const express    = require('express');
+const authRoutes = require('./routes/api/authRoutes');
 
 const app     = express();
 const port    = process.env.PORT;
 
-app.use(express.json());                    // Parse incoming requests as json
+app.use(express.json());                                // Parse incoming requests as json
+app.use('/api/auth', authRoutes);                       // Mount authRoutes at /api/auth endpoint
 
 app.listen(port, () => {
 
