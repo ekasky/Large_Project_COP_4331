d4217676527218754e417b245369eb3fb6bf0f75
Evan Kasky <ekasky25@gmail.com>
2024-06-16 15:28:32 -0400

    Reset 	- Installs project dependencies

--- server/routes/api/authRoutes.js
+++
@@ -1,7 +0,0 @@
-const express                = require('express');
-const router                 = express.Router();
-const { registerController } = require('../../controllers/authControllers');
-
-router.post('/register', registerController);
-
-module.exports = router;
