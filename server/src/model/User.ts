ba113460fcf428199d04cb4a086fb78fb9060a8b
Evan Kasky <ekasky25@gmail.com>
2024-06-16 15:59:22 -0400

    Adds a Mongoose model for User's with well defined types

---
+++ server/src/model/User.ts
@@ -0,0 +1,61 @@
+import { Document, Schema, model } from "mongoose";
+
+// Interface representing a User document in MongoDB
+export interface UserDocument extends Document {
+
+    first_name: string;
+    last_name: string;
+    email: string;
+    username: string;
+    password: string;
+    createdAt: Date;
+    lastLogin: Date;
+    emailVerified: boolean;
+
+
+};
+
+// Create the User Schema
+const userSchema = new Schema<UserDocument>({
+
+    first_name: {
+        type: String,
+        required: true
+    },
+
+    last_name: {
+        type: String,
+        required: true
+    },
+
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+
+    password: {
+        type: String,
+        required: true
+    },
+
+    createdAt: {
+        type: Date,
+        default: Date.now()
+    },
+
+    lastLogin: {
+        type: Date,
+        default: null
+    }
+
+});
+
+// Create and export the User model
+export const User = model<UserDocument>('User', userSchema);
