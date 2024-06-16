320de13d4d4ad32ba3382a33e48a9655e0a26cfa
Evan Kasky <ekasky25@gmail.com>
2024-06-16 16:37:02 -0400

    Checks for reuqired fields in register controller

--- server/src/controllers/registerController.ts
+++ server/src/controllers/registerController.ts
@@ -1,10 +1,22 @@
 import { Request, Response } from "express";
+import checkRequiredFields from "../utils/checkRequiredFields";
 
 const registerController = async (req:Request, res:Response) => {
 
     // Extract the request body from the request
     const {first_name, last_name, email, username, password}:{first_name: string, last_name:string, email:string, username:string, password:string} = req.body;
-    
+
+    // Check if the required fields are present in the request body
+    const missing = checkRequiredFields(req, ["first_name", "last_name", "email", "username", "password"]);
+
+    if(missing !== null) {
+
+        return res.status(400).json({
+            message: "Missing required fields",
+            missing: missing
+        });
+
+    }
 
 };
 
