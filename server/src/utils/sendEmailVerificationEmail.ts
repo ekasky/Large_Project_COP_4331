5762951a7051dcdaedce9a6372fc0587bd6cca69
Evan Kasky <ekasky25@gmail.com>
2024-06-16 20:13:49 -0400

    Configures node mailer

--- server/src/utils/sendEmailVerificationEmail.ts
+++ server/src/utils/sendEmailVerificationEmail.ts
@@ -17,6 +17,26 @@ const sendEmailVerificationEmail = (email:string) => {
         purpose: 'Account verification'
     }, JWT_KEY, {algorithm: 'HS256'});
 
+    // Get user, pass, and base domain
+    const auth_email_address = process.env.EMAIL_ADDRESS;
+    const auth_password = process.env.EMAIL_PASSWORD;
+    const base_url = process.env.BASE_URL;
+
+    // Create the verification link
+    const link = `${base_url}/verify-account?token=${token}`;
+
+
+    // Configure nodemailer
+    const transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
+        auth: {
+            user: auth_email_address,
+            pass: auth_password
+        }
+    });
+
 };
 
 export default sendEmailVerificationEmail;
