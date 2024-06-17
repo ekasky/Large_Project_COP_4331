fea2c1d2dcc35b670912777cc05d65f43ce8f0c7
Evan Kasky <ekasky25@gmail.com>
2024-06-16 21:18:03 -0400

    Adds verify email route

---
+++ server/src/controllers/verifyEmailController.ts
@@ -0,0 +1,9 @@
+import { Request, Response } from "express"
+
+const verifyEmailController = async (req:Request, res:Response) => {
+
+    
+
+}
+
+export default verifyEmailController;
--- server/src/routes/authRoutes.ts
+++ server/src/routes/authRoutes.ts
@@ -1,8 +1,10 @@
 import express, {Request, Response} from "express";
 import registerController from "../controllers/registerController";
+import verifyEmailController from "../controllers/verifyEmailController";
 
 const router = express.Router();
 
 router.post('/register', registerController);
+router.get('/verify-email', verifyEmailController);
 
 export default router;
