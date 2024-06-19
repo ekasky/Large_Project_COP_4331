2002bac7722728c6f349cf068ff2c7034add2065
Evan Kasky <ekasky25@gmail.com>
2024-06-18 23:27:36 -0400

    Adds temporary reset-password page until react page is done

--- src/server.ts
+++ src/server.ts
@@ -17,6 +17,43 @@ app.use(express.json());
 // Connect to MongoDB
 connectToDb();
 
+/* 
+    Reset Password Page
+    TEMPORARY: WILL BE REPLACED BY A REACT PAGE
+*/
+app.get("/reset-password", (req, res) => {
+
+    const html = `
+    
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Reset Password</title>
+    </head>
+    <body>
+        <form id="resetPasswordForm" action="/reset-password" method="POST">
+            <div class="form-group">
+                <label for="newPassword">New Password:</label>
+                <input type="password" id="newPassword" name="newPassword" required>
+            </div>
+            <div class="form-group">
+                <label for="confirmPassword">Confirm Password:</label>
+                <input type="password" id="confirmPassword" name="confirmPassword" required>
+            </div>
+            <button type="submit">Reset Password</button>
+        </form>
+    </body>
+    </html>
+
+
+    `;
+
+    res.send(html);
+
+});
+
 // Add auth routes
 app.use("/api/auth", authRoutes);
 
