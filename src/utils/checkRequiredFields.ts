a274c335685fea9c3fa6c43de36d832dc7ba1c7c
Evan Kasky <ekasky25@gmail.com>
2024-06-19 17:11:47 -0400

    Adds type checks to request input
    	- Along with making sure the user has all the required fields it
    	  also ensures the types are correct

--- src/utils/checkRequiredFields.ts
+++ src/utils/checkRequiredFields.ts
@@ -1,11 +1,55 @@
+import { Request } from "express";
+
+export interface ExpectedFields {
+
+    name: string;
+    type: 'string' | 'number' | 'boolean';
+
+};
+
+export interface ExpectedFieldsReturn {
+
+    missingFields: string[],
+    incorrectTypes: {
+        field: string,
+        expected: string
+    }[]
+
+};
+
+const checkRequiredFields = (req:Request, expectedFields:ExpectedFields[]):ExpectedFieldsReturn | null => {
+
+    const result:ExpectedFieldsReturn = {
+        missingFields: [],
+        incorrectTypes: []
+    };
+
+    expectedFields.forEach( field => {
+
+        // Check if the required field is in the request bosy
+        if( !(field.name in req.body) ) {
+
+            result.missingFields.push(field.name);
+
+        }
+        else {
+
+            if(typeof req.body[field.name] !== field.type) {
+
+                result.incorrectTypes.push({
+                    field: field.name,
+                    expected: field.type
+                });
+
+            }
+
+        }
+
+    });
 
 
-const checkRequiredFields = (req:any, expectedFields:string[]):string[] | null => {
-
-    const missingFields = expectedFields.filter(field => !req.body[field]); 
-
-    if(missingFields.length > 0) {
-        return missingFields;
+    if(result.missingFields.length > 0 || result.incorrectTypes.length > 0) {
+        return result;
     }
 
     return null;
