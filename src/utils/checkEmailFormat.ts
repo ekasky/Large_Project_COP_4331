18d6e7d4e69673bf3ff80305aa577763ad9359a4
Evan Kasky <ekasky25@gmail.com>
2024-06-19 19:08:17 -0400

    Updates checkEmailFormat function
    	- Removes whitespace from address
    
    	- Sanitize the input
    		* Convert all to lowercase
    		* Removes any white space
    		* Canonical Form for email address

--- src/utils/checkEmailFormat.ts
+++ src/utils/checkEmailFormat.ts
@@ -1,9 +1,28 @@
 import validator from "validator";
 
-const checkEmailFormat = (email:string):boolean => {
+const MAX_EMAIL_LENGTH = 100;
 
-    return validator.isEmail(email);
+const checkEmailFormat = (email:string):string | null => {
 
+    // Trim leading and trailing spaces
+    email = email.trim();
+
+    // Sanitize the email input
+    email = validator.normalizeEmail(email) || '';
+
+    if( !email || !validator.isEmail(email) ) {
+
+        return 'Invalid email address';
+
+    }
+
+    if(email.length > MAX_EMAIL_LENGTH) {
+
+        return "Email address too long";
+
+    }
+
+    return null;
 };
 
 export default checkEmailFormat;
