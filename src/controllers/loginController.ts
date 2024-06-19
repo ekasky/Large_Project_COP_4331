7d0d9bb9ba9554759ca89e4ea1e380241e49598c
Evan Kasky <ekasky25@gmail.com>
2024-06-18 20:32:12 -0400

    Adds login endpoint

--- src/controllers/loginController.ts
+++ src/controllers/loginController.ts
@@ -1,7 +1,10 @@
 import { Request, Response } from "express";
+import bcrypt from "bcrypt";
+import jsonwebtoken from "jsonwebtoken";
 import checkRequiredFields from "../utils/checkRequiredFields";
+import findUserByUsername from "../utils/findUserByUsername";
 
-const loginController = (req:Request, res:Response) => {
+const loginController = async (req:Request, res:Response) => {
 
     // Extract the user's login credentials
     const { username, password } = req.body;
@@ -17,6 +20,49 @@ const loginController = (req:Request, res:Response) => {
         });
 
     }
+
+    // Attempt to find the user by their username
+    const user = await findUserByUsername(username);
+
+    if(user === null) {
+
+        return res.status(401).json({
+            message: "Invalid Login Credentials"
+        });
+
+    }
+
+    // Compare the password in the user document to the password supplied in the request
+    const valid = await bcrypt.compare(password, user.password);
+
+    if(!valid) {
+
+        return res.status(401).json({
+            message: "Invalid Login Credentials"
+        });
+
+    }
+
+    // Get JWT key from env
+    const JWT_KEY = process.env.JWT_PRIVATE_KEY as string;
+
+    // Prepare JWT Data
+    const iat = Math.floor(Date.now() / 1000); // Issued at in seconds
+    const exp = iat + (2 * 60 * 60); // Expiration time in seconds (2 hours from now)
+    const id  = user.id;
+
+    // Create a jwt for the user's session
+    const token = jsonwebtoken.sign({
+        iat,
+        exp,
+        id,
+        username
+    }, JWT_KEY, {algorithm: 'HS256'} );
+
+    // Return user token to the browser
+    return res.status(200).json({
+        token
+    });
 
 };
 
