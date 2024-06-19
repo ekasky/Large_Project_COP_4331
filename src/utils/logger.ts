d22cb27f7bdee173a17695bb3c27b63e3ec9f94c
Evan Kasky <ekasky25@gmail.com>
2024-06-19 16:35:44 -0400

    Removes parts of code from when logger was middleware
    	- Logs immediatley now

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -4,33 +4,27 @@ import express, { Request, Response, NextFunction } from "express";
 
 const logFilePath = path.resolve(__dirname, '../../server.log');
 
-const logger = (req:Request, res:Response, message:string):void => {
+const logger = (req:Request, message:string, level:string="INFO"):void => {
 
-    const start = Date.now();
 
-    res.on('finish', () => {
+    const timestamp = new Date().toISOString();
+    const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress || null;
+    const method = req.method;
+    const url = req.originalUrl;
+    const userAgent = req.headers['user-agent'] || 'unknown';
+    const logMessage = `${timestamp} -- IP ADDRESS: ${ip} -- METHOD: ${method} -- URL: ${url} -- USER AGENT: ${userAgent} -- MESSAGE: [${level}]: ${message}\n`;
 
-        const responseTime = Date.now() - start;
-        const timestamp = new Date().toISOString();
-        const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress || null;
-        const method = req.method;
-        const url = req.originalUrl;
-        const code = res.statusCode;
-        const userAgent = req.headers['user-agent'] || 'unknown';
-        const logMessage = `${timestamp} -- IP ADDRESS: ${ip} -- METHOD: ${method} -- URL: ${url} -- STATUS CODE: ${code} -- USER AGENT: ${userAgent} -- RESPONSE TIME: ${responseTime}ms -- MESSAGE: ${message}\n`;
+    fs.appendFile(logFilePath, logMessage, (error) => {
 
-        fs.appendFile(logFilePath, logMessage, (error) => {
-
-            if(error) {
-                console.error('ERROR WRITING TO THE LOG FILE:', error);
-            }
-
-        });
-
-        // Log to the console as well
-        console.log(logMessage);
+        if(error) {
+            console.error('ERROR WRITING TO THE LOG FILE:', error);
+        }
 
     });
+
+    // Log to the console as well
+    console.log(logMessage);
+
 
 };
 
