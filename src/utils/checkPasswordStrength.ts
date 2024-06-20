6fd0c56571fbc5f5ddb4c3d7639fbe36bd33d990
Evan Kasky <ekasky25@gmail.com>
2024-06-19 20:08:47 -0400

    Updates checkPasswordStrength function

--- src/utils/checkPasswordStrength.ts
+++ src/utils/checkPasswordStrength.ts
@@ -1,18 +1,29 @@
 import validator from "validator";
 import { PasswordRequirements } from "./interfaces";
 
-const checkPasswordStrength = (password:string):boolean => {
+const passwordRequirements:PasswordRequirements = {
+    minLength: 8,
+    maxLength: 64,
+    minLowerCase: 1,
+    minUpperCase: 1,
+    minNumbers: 1,
+    minSymbols: 1
+};
 
-    // Define password requirements
-    const passwordRequirements:PasswordRequirements = {
-        minLength: 8,
-        minLowerCase: 1,
-        minUpperCase: 1,
-        minNumbers: 1,
-        minSymbols: 1
-    };
+const checkPasswordStrength = (password:string):string | null => {
 
-    return validator.isStrongPassword(password, passwordRequirements);
+    
+    // Trim leading and trailing spaces
+    password = password.trim();
+
+    // Check if password meets the min length requirment
+    if(password.length < passwordRequirements.minLength || password.length > passwordRequirements.maxLength || !validator.isStrongPassword(password, passwordRequirements)) {
+
+        return `Password must be at least ${passwordRequirements.minLength} characters long, not exceed ${passwordRequirements.maxLength} characters, contain at least ${passwordRequirements.minLowerCase} lowercase letter${passwordRequirements.minLowerCase > 1 ? 's' : ''}, ${passwordRequirements.minUpperCase} uppercase letter${passwordRequirements.minUpperCase > 1 ? 's' : ''}, ${passwordRequirements.minNumbers} number${passwordRequirements.minNumbers > 1 ? 's' : ''}, and ${passwordRequirements.minSymbols} symbol${passwordRequirements.minSymbols > 1 ? 's' : ''}.`;
+
+    }
+
+    return null;
 
 };
 
--- src/utils/interfaces.ts
+++ src/utils/interfaces.ts
@@ -1,5 +1,6 @@
 export interface PasswordRequirements {
     minLength: number,
+    maxLength: number,
     minLowerCase: number,
     minUpperCase: number,
     minNumbers: number,
